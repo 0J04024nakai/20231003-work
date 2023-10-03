39ad659589f352886bcca69385c72d487a11c6d0
NAKAI ZEN <kd1293049@st.kobedenshi.ac.jp>
2023-10-03 12:19:42 +0900

    ex2

--- ex2/work.py
+++ ex2/work.py
@@ -2,7 +2,16 @@ from math import ceil
 
 def calc_account(m):
     # 実装は入れていません、自分で入れてください
-    pass
+    if m <= 0:
+        return None
+    elif m <= 1700:
+        return 610
+    # 1700mを超えるとその後は 315m毎に 80円増えるものとする
+    
+    fare = 610 + 80 * ceil((m - 1700) / 315 )
+    return fare
+
+    
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
